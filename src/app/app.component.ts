e5a8a280b530c0a5a5c8d4488beba22c678dd4e8
Patrick Sun <patrick@lawipac.com>
2022-12-16 19:05:24 +1100

    initial kendo ui button added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+
+  onClick( event: any) {
+    console.log("button clicked", event);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { InputsModule } from '@progress/kendo-angular-inputs';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LabelModule } from '@progress/kendo-angular-label';
+import { IconsModule } from '@progress/kendo-angular-icons';
+import {ButtonsModule} from "@progress/kendo-angular-buttons";
+
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    InputsModule,
+    BrowserAnimationsModule,
+    LabelModule,
+    IconsModule,
+    ButtonsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
