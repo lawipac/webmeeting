52ac77c4803c337839ab4a7279d08c08f20a73da
Patrick Sun <patrick@lawipac.com>
2023-01-10 22:05:25 +1100

    login page seems working and https request data type has been implemented

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   apiKey: 'biukop-is-helping-superforex',
-  apiBaseUrl: 'https://api.webtradepay.au/v1/',
+  apiBaseUrl: 'https://api.webtradepay.au/v1',
   wss: "wss://wss.webtradepay.au/meeting-prod"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
-  production: true,
-  apiKey: 'defaultKey',
+  production: false,
+  apiKey: 'biukop-is-helping-superforex',
+  apiBaseUrl: 'https://api.webtradepay.au/v1-dev',
   wss: "wss://wss.webtradepay.au/meeting-dev"
-};
+}; // this is the same as environment.dev.ts ---
