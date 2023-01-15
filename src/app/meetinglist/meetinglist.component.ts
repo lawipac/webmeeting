e5a7c763ed544290aadea5e82ae48f07a2ef78a8
Patrick Sun <patrick@lawipac.com>
2023-01-15 16:56:30 +1100

    fake login with create meeting seems working

---
+++ src/app/meetinglist/meetinglist.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-meetinglist',
+  templateUrl: './meetinglist.component.html',
+  styleUrls: ['./meetinglist.component.scss']
+})
+export class MeetinglistComponent {
+
+}
