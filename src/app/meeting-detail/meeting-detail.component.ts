4267322e5c8ad7dfae098393b9d464e13a1fbaf9
Patrick Sun <patrick@lawipac.com>
2023-01-18 02:43:25 +1100

    crud meeting ready

---
+++ src/app/meeting-detail/meeting-detail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-meeting-detail',
+  templateUrl: './meeting-detail.component.html',
+  styleUrls: ['./meeting-detail.component.scss']
+})
+export class MeetingDetailComponent {
+
+}
