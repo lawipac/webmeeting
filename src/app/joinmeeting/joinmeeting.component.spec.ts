2b19ddc0bfd2010f46e91d75cba59c1373c8bc95
Patrick Sun <patrick@lawipac.com>
2023-01-31 22:29:29 +1100

    start direct join meeting

---
+++ src/app/joinmeeting/joinmeeting.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { JoinmeetingComponent } from './joinmeeting.component';
+
+describe('JoinmeetingComponent', () => {
+  let component: JoinmeetingComponent;
+  let fixture: ComponentFixture<JoinmeetingComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ JoinmeetingComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(JoinmeetingComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/joinmeeting/joinmeeting.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-joinmeeting',
+  templateUrl: './joinmeeting.component.html',
+  styleUrls: ['./joinmeeting.component.scss']
+})
+export class JoinmeetingComponent {
+
+}
